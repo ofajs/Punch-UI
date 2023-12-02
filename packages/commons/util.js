fc0a14e7c7109706f8d76fade29e635a48e2d263
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-12-02 15:56:15 +0800

    feat: add translucent color

--- packages/commons/util.js
+++ packages/commons/util.js
@@ -1,14 +1,16 @@
-export function convertToRGBA(colorString, opacity = "0.1") {
+export function convertToRGBA(colorString, opacity = 20) {
   if (colorString.startsWith("#")) {
     const hexColor = colorString.slice(1);
     const red = parseInt(hexColor.slice(0, 2), 16);
     const green = parseInt(hexColor.slice(2, 4), 16);
     const blue = parseInt(hexColor.slice(4, 6), 16);
 
-    const rgbaColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+    const rgbaColor = `rgba(${red}, ${green}, ${blue}, ${opacity / 100})`;
     return rgbaColor;
   } else if (colorString.startsWith("rgb")) {
-    return colorString.replace("rgb", "rgba").replace(")", `, ${opacity})`);
+    return colorString
+      .replace("rgb", "rgba")
+      .replace(")", `, ${opacity / 100})`);
   } else {
     return "";
   }
@@ -20,8 +22,8 @@ export function convertToRGBA(colorString, opacity = "0.1") {
 // const rgbaColor1 = convertToRGBA(color1);
 // const rgbaColor2 = convertToRGBA(color2);
 
-// console.log(rgbaColor1); // 输出 "rgba(255, 87, 51, 0.1)"
-// console.log(rgbaColor2); // 输出 "rgba(120, 45, 200, 0.1)"
+// console.log(rgbaColor1); // 输出 "rgba(255, 87, 51, 0.2)"
+// console.log(rgbaColor2); // 输出 "rgba(120, 45, 200, 0.2)"
 
 export const setMainColor = (target, color) => {
   target.style.setProperty("--main-color", `var(--md-sys-color-${color})`);
@@ -37,5 +39,13 @@ export const setMainColor = (target, color) => {
   target.style.setProperty(
     "--on-main-color-container",
     `var(--md-sys-color-on-${color}-container)`
+  );
+  target.style.setProperty(
+    "--main-hover-color",
+    `var(--md-ref-palette-translucent-${color}60)`
+  );
+  target.style.setProperty(
+    "--wave-color",
+    `var(--md-ref-palette-translucent-${color}50)`
   );
 };
