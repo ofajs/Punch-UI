b9bcbba3731bbe6e747e92060ec141f3b74925dd
Yao <kirayao@hotmail.com>
2023-12-04 22:49:01 +0800

    feat: add slider component (#30)
    * feat: opt code
    
    * refactor: rename color var
    
    * feat: add init slider
    
    * feat: add slider bubble
    
    * fix: slider step
    
    * fix: slider safe area
    
    * fix: slider step
    
    * feat: add slider cursor mark
    
    * feat: add p-slider-mark
    
    * feat: expand slider click area
    
    * Update all.html
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- packages/commons/util.js
+++ packages/commons/util.js
@@ -1,47 +1,30 @@
-export function convertToRGBA(colorString, opacity = 20) {
-  if (colorString.startsWith("#")) {
-    const hexColor = colorString.slice(1);
-    const red = parseInt(hexColor.slice(0, 2), 16);
-    const green = parseInt(hexColor.slice(2, 4), 16);
-    const blue = parseInt(hexColor.slice(4, 6), 16);
-
-    const rgbaColor = `rgba(${red}, ${green}, ${blue}, ${opacity / 100})`;
-    return rgbaColor;
-  } else if (colorString.startsWith("rgb")) {
-    return colorString
-      .replace("rgb", "rgba")
-      .replace(")", `, ${opacity / 100})`);
-  } else {
-    return "";
-  }
-}
-
-// const color1 = "#FF5733";
-// const color2 = "rgb(120, 45, 200)";
-
-// const rgbaColor1 = convertToRGBA(color1);
-// const rgbaColor2 = convertToRGBA(color2);
-
-// console.log(rgbaColor1); // 输出 "rgba(255, 87, 51, 0.2)"
-// console.log(rgbaColor2); // 输出 "rgba(120, 45, 200, 0.2)"
-
 export const setMainColor = (target, color) => {
-  target.style.setProperty("--main-color", `var(--md-sys-color-${color})`);
+  if (!color) {
+    target.style.setProperty("--pui-main-color", ``);
+    target.style.setProperty("--pui-on-main-color", ``);
+    target.style.setProperty("--pui-main-color-container", ``);
+    target.style.setProperty("--pui-on-main-color-container", ``);
+    target.style.setProperty("--pui-main-hover-color", ``);
+    target.style.setProperty("--wave-color", ``);
+    return;
+  }
+
+  target.style.setProperty("--pui-main-color", `var(--md-sys-color-${color})`);
   target.style.setProperty(
-    "--on-main-color",
+    "--pui-on-main-color",
     `var(--md-sys-color-on-${color})`
   );
 
   target.style.setProperty(
-    "--main-color-container",
+    "--pui-main-color-container",
     `var(--md-sys-color-${color}-container)`
   );
   target.style.setProperty(
-    "--on-main-color-container",
+    "--pui-on-main-color-container",
     `var(--md-sys-color-on-${color}-container)`
   );
   target.style.setProperty(
-    "--main-hover-color",
+    "--pui-main-hover-color",
     `var(--md-ref-palette-translucent-${color}60)`
   );
   target.style.setProperty(
--- packages/init.js
+++ packages/init.js
@@ -7,4 +7,16 @@
       "@pui": packagesUrl,
     },
   });
+
+  // 初始化颜色
+  const color = "primary";
+  $("head").push(
+    `<style>:root{
+      --pui-main-color:var(--md-sys-color-${color});
+      --pui-on-main-color:var(--md-sys-color-on-${color});
+      --pui-main-color-container:var(--md-sys-color-${color}-container);
+      --pui-on-main-color-container:var(--md-sys-color-on-${color}-container);
+      --pui-main-hover-color:var(--md-ref-palette-translucent-${color}60);
+}</style>`
+  );
 })();
