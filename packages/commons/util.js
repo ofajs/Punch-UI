956ebda4b51f6fea10bafdac10fdda21688a658f
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-12-04 22:25:56 +0800

    feat: add p-slider-mark

--- packages/commons/util.js
+++ packages/commons/util.js
@@ -1,21 +1,3 @@
-export function convertToRGBA(colorString, opacity = 20) {
-  if (colorString.startsWith("#")) {
-    const hexColor = colorString.slice(1);
-    const red = parseInt(hexColor.slice(0, 2), 16);
-    const green = parseInt(hexColor.slice(2, 4), 16);
-    const blue = parseInt(hexColor.slice(4, 6), 16);
-
-    const rgbaColor = `rgba(${red}, ${green}, ${blue}, ${opacity / 100})`;
-    return rgbaColor;
-  } else if (colorString.startsWith("rgb")) {
-    return colorString
-      .replace("rgb", "rgba")
-      .replace(")", `, ${opacity / 100})`);
-  } else {
-    return "";
-  }
-}
-
 export const setMainColor = (target, color) => {
   if (!color) {
     target.style.setProperty("--pui-main-color", ``);
