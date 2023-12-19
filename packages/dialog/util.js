5615e568b2fe9b670eb877dd6774665f3d182eb6
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-12-19 23:20:43 +0800

    feat: add confirm + alert + prompt

---
+++ packages/dialog/util.js
@@ -0,0 +1,104 @@
+let maskId = 10001;
+
+const load = lm(import.meta.url);
+
+const appendDialog = (code, clickYes) => {
+  return new Promise((resolve) => {
+    const dialog = $(code);
+
+    const closeDialog = () => {
+      dialog.open = false;
+      setTimeout(() => {
+        dialog.remove();
+      }, 400);
+    };
+
+    dialog.$(".dialog-yes").on("click", () => {
+      if (clickYes) {
+        clickYes({ dialog, resolve });
+      } else {
+        resolve(true);
+      }
+      closeDialog();
+    });
+    dialog.$(".dialog-cancel") &&
+      dialog.$(".dialog-cancel").on("click", () => {
+        resolve(false);
+        closeDialog();
+      });
+    dialog.on("click-mask", () => {
+      resolve(null);
+      closeDialog();
+    });
+    dialog.on("click-close", () => {
+      resolve(null);
+      closeDialog();
+    });
+
+    // setTimeout(, 100);
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        dialog.open = true;
+      }, 10);
+    });
+
+    $("body").push(dialog);
+  });
+};
+
+export default {
+  get maskId() {
+    return maskId;
+  },
+  set maskId(val) {
+    maskId = val;
+  },
+  async confirm(opts) {
+    await load("./dialog.html");
+
+    return appendDialog(`
+  <p-dialog fullscreen="540">
+    <span slot="title">${opts.title}</span>
+    ${opts.content}
+    <p-button variant="text" class="dialog-cancel" slot="bottom">${
+      opts.cancel || "Cancel"
+    }</p-button>
+    <p-button slot="bottom" class="dialog-yes">${opts.yes || "Yes"}</p-button>
+  </p-dialog>`);
+  },
+  async alert(opts) {
+    await load("./dialog.html");
+
+    return appendDialog(`
+  <p-dialog fullscreen="540">
+    <span slot="title">${opts.title}</span>
+    ${opts.content}
+    <p-button variant="text" class="dialog-yes" slot="bottom">${
+      opts.ok || "OK"
+    }</p-button>
+  </p-dialog>`);
+  },
+  async prompt(opts) {
+    await load("./dialog.html");
+    await load("../text-field/text-field.html");
+
+    return appendDialog(
+      `
+  <p-dialog fullscreen="540">
+    <span slot="title">${opts.title}</span>
+    <div style="padding:8px 0">
+      <p-text-field style="--label-bg:var(--md-sys-color-on-normal);width:100%;">
+        ${opts.label ? `<label>${opts.label}</label>` : ""}
+      </p-text-field>
+    </div>
+    <p-button variant="text" class="dialog-cancel" slot="bottom">${
+      opts.cancel || "Cancel"
+    }</p-button>
+    <p-button slot="bottom" class="dialog-yes">${opts.yes || "Yes"}</p-button>
+  </p-dialog>`,
+      ({ dialog, resolve }) => {
+        resolve(dialog.$("p-text-field").value);
+      }
+    );
+  },
+};
