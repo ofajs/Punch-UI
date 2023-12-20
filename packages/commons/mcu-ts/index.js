6808f6897104941f29a9fb278f1cf0137ab40b2c
Yao <kirayao@hotmail.com>
2023-12-20 18:18:52 +0800

    feat: add drawer component (#43)
    * feat: add drawer component
    
    * feat: support pos attr
    
    * refactor: rename pos to anchor
    
    * feat: color system add surface
    
    * fix: text-field label bg
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- packages/commons/mcu-ts/index.js
+++ packages/commons/mcu-ts/index.js
@@ -1,9 +1,9 @@
 import { Hct } from "./hct/hct.js";
 
 // material v3 主要的颜色阶段
-const points = [100, 98, 95, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0];
+const gloPoints = [100, 98, 95, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0];
 
-export const getColors = (color) => {
+export const getColors = (color, points = gloPoints) => {
   const hctObj = new Hct(parseInt(`0xff${color.replace("#", "")}`));
 
   return points.map((tone) => {
