224422fd8ce5e18c7a39c2682844cbede6166f68
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-01-08 17:50:28 +0800

    fix: some issuse

--- init.js
+++ init.js
@@ -1,10 +1,19 @@
-(async () => {
+export default (async () => {
   const packagesUrl = import.meta.url.replace(/(.+)\/.+/, "$1");
 
   // 初始化逻辑
   lm.config({
     alias: {
-      "@pui": packagesUrl,
+      "@puiroot": `${packagesUrl}`,
+      "@pui": `${packagesUrl}/packages`,
     },
   });
+
+  const targetLink = $(`link[href*="${packagesUrl}/packages/css/public.css"]`);
+
+  if (!targetLink) {
+    $("head").push(
+      `<link rel="stylesheet" href="${packagesUrl}/packages/css/public.css">`
+    );
+  }
 })();
