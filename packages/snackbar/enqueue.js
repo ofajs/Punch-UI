e5e9edf4b41cb6fdb34255284b412164a04e7352
Yao <kirayao@hotmail.com>
2024-03-04 23:50:30 +0800

    fix: issuse about dark mode (#67)
    * fix: enqueue time
    
    * Update enqueue.js
    
    * refactor: remove useless code
    
    * fix: dark mode code
    
    * chore: bump version
    
    * Create package-lock.json
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- packages/snackbar/enqueue.js
+++ packages/snackbar/enqueue.js
@@ -6,7 +6,7 @@ $("head").push(`<style>
   width:calc(100% - var(--enqueue-container-left, 20px));
   height:calc(100% - var(--enqueue-container-bottom, 20px));
   max-width:600px;
-  z-index:10;
+  z-index:400;
   display:flex;
   box-sizing: border-box;
   flex-direction:column;
@@ -65,7 +65,7 @@ export default async function enqueue(opts) {
   const defaults = {
     content: "",
     buttons: [],
-    time: 300000,
+    time: 3000,
     click: () => {},
     close: () => {},
   };
