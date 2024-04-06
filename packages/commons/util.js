fbd6d04c915a266d32856bcd1b84a9e0b72ea9b2
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-04-06 16:19:54 +0800

    Update util.js

--- packages/commons/util.js
+++ packages/commons/util.js
@@ -2,27 +2,19 @@ export const setMainColor = (target, color) => {
   if (!color) {
     color = "primary";
   }
+  let themeStyle = target.shadow.$("#pui-theme-color");
 
-  target.style.setProperty("--pui-main-color", `var(--md-sys-color-${color})`);
-  target.style.setProperty(
-    "--pui-on-main-color",
-    `var(--md-sys-color-on-${color})`
-  );
+  if (!themeStyle) {
+    themeStyle = $(`<style id="pui-theme-color"></style>`);
+    target.shadow.push(themeStyle);
+  }
 
-  target.style.setProperty(
-    "--pui-main-color-container",
-    `var(--md-sys-color-${color}-container)`
-  );
-  target.style.setProperty(
-    "--pui-on-main-color-container",
-    `var(--md-sys-color-on-${color}-container)`
-  );
-  target.style.setProperty(
-    "--pui-main-hover-color",
-    `var(--md-ref-palette-translucent-${color}60)`
-  );
-  target.style.setProperty(
-    "--wave-color",
-    `var(--md-ref-palette-translucent-${color}50)`
-  );
+  themeStyle.html = `:host{
+    --pui-main-color: var(--md-sys-color-${color});
+    --pui-on-main-color: var(--md-sys-color-on-${color});
+    --pui-main-color-container: var(--md-sys-color-${color}-container);
+    --pui-on-main-color-container: var(--md-sys-color-on-${color}-container);
+    --pui-main-hover-color: var(--md-ref-palette-translucent-${color}60);
+    --wave-color: var(--md-ref-palette-translucent-${color}50);
+  }`;
 };
