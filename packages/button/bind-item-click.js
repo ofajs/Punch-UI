c4b96f0ff4fefe72dd4e74fc51457ccc1e2d85c4
Yao <kirayao@hotmail.com>
2024-05-18 14:53:55 +0800

    fix: more issuse (#71)
    * fix: tab active icon
    
    * fix: some isssue
    
    * fix: issuse about hover
    
    * fix: carousel component
    
    * refactor: remove useless code
    
    * fix: bind item click
    
    * Update text-field.html
    
    * fix: add placeholder
    
    * feat: drawer component support dialog-not-mobile
    
    * fix: more style issuse
    
    * fix: dialog style
    
    * fix: some issuse
    
    * Update dialog.html
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- packages/button/bind-item-click.js
+++ packages/button/bind-item-click.js
@@ -1,18 +1,32 @@
-export default function bindItemClick(_this, success) {
+export default function bindClick(_this, success) {
   _this.on("click", (e) => {
-    if (_this.disabled === null) {
-      const notSlotEls = _this.filter((e) => e.is(`:not([slot])`));
-      if (
-        notSlotEls.length === 1 &&
-        notSlotEls[0].tag === "a" &&
-        e.target.tagName !== "A"
-      ) {
-        notSlotEls[0].ele.dispatchEvent(new PointerEvent("click"));
+    if (_this.disabled !== null) {
+      e.stopImmediatePropagation();
+      return;
+    }
+
+    const notSlotEls = _this.filter((e) => e.is(`:not([slot])`));
+    if (notSlotEls.length === 1 && notSlotEls[0].tag === "a") {
+      if (e.target.tagName === "A") {
+        return;
       }
 
-      success && success();
-    } else {
       e.stopImmediatePropagation();
+
+      if (navigator.userAgent.includes("Firefox")) {
+        notSlotEls[0].ele.click();
+      } else {
+        notSlotEls[0].ele.dispatchEvent(
+          new PointerEvent("click", {
+            metaKey: e.metaKey,
+            shiftKey: e.shiftKey,
+            bubbles: e.bubbles,
+            cancelable: true,
+          })
+        );
+      }
     }
+
+    success && success();
   });
 }
