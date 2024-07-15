f09c28e27d3d5d7e79d6bbccdbc49a4cfc67aca7
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-16 00:29:29 +0800

    chore: update ofa.js

--- init.js
+++ init.js
@@ -7,19 +7,3 @@ lm.config({
     "@pui": `${packagesUrl}/packages`,
   },
 });
-
-const targetLink = $(`link[href*="${packagesUrl}/packages/css/public.css"]`);
-
-if (!targetLink) {
-  const themeLink = $(`link[pui-theme]`);
-
-  if (themeLink) {
-    themeLink.before(
-      `<link rel="stylesheet" href="${packagesUrl}/packages/css/public.css">`
-    );
-  } else {
-    $("head").push(
-      `<link rel="stylesheet" href="${packagesUrl}/packages/css/public.css">`
-    );
-  }
-}
