7f94a3a33551d327b9afd3a28058d9612da08370
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-03 14:57:05 +0800

    refactor: remove useless code

--- packages/commons/mcu-ts/readme.md
+++
@@ -1,3 +0,0 @@
-# mcu-ts
-
-This algorithm was copied and modified from [material-color-utilities](https://github.com/material-foundation/material-color-utilities/tree/main/typescript)
--- packages/util.js
+++
@@ -1,8 +0,0 @@
-import dialogUtil from "./dialog/command.js";
-import enqueue from "./snackbar/enqueue.js";
-
-export const confirm = dialogUtil.confirm;
-export const alert = dialogUtil.alert;
-export const prompt = dialogUtil.prompt;
-
-export { enqueue };
